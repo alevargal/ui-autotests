d71764aa34ebd8a76e5dc49ebbb8395c011f6669
Alex <alevargal97@gmail.com>
2022-12-09 22:55:48 +0300

    last change

--- src/test/java/autotests.java
+++ src/test/java/autotests.java
@@ -1,19 +1,10 @@
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-
-import java.util.concurrent.TimeUnit;
 
 public class autotests {
 
     @Test
-    public void test(){
-        System.setProperty("webdriver.chrome.driver", "/Users/maxwell/Мое/Автотесты/ui-autotests/src/test/resources/chromedriver");
-        WebDriver driver = new ChromeDriver();
-//        System.setProperty("webdriver.chrome.driver", "/Users/maxwell/Downloads/chromedriver_mac64/chromedriver.exe");
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driver.get("https://cristalblue.ru/");
-
+    public void test() {
+        System.out.printf("Hello world!");
     }
 
 }
